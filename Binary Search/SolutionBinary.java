da0047f3b2618f60b93c619c54f46b5f276cb49a
maolintuntun <39028815+maolintuntun@users.noreply.github.com>
2019-06-13 23:59:23 +0800

    Update SolutionBinary.java

--- Binary Search/SolutionBinary.java
+++ Binary Search/SolutionBinary.java
@@ -1,5 +1,25 @@
 package binarysearch;
 
 public class SolutionBinary {
+	int search(int[] array, int target){
+		if(array == null||array.length==0){
+			return -1;
+		}
+		int left = 0, right = array.length - 1;
+		while(left <= right){
+			int mid = left + (right - left)/2;
+			
+			if(target == array[mid]){
+				return mid;
+			}else if(target < array[mid]){
+				right = mid - 1;
+			}else{
+				left = mid + 1;
+			}
+			
+			
+		}
+		return -1;
+	}
 
 }
